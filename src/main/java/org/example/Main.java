0f4416eb896ecba2007c99621ac361a42a1d5f01
vladimir <vladimir.volkov-2013@yandex.ru>
2024-01-13 21:38:39 +0300

    added Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.example.dao.FlightBuilder;
+import org.example.model.Flight;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Flight> flights = FlightBuilder.createFlights();
+        System.out.printf(flights.toString());
     }
 }
